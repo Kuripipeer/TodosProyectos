56ac6d656a00cb03fab4814cf80570553e98aa1a
Kuripipeer <mchristianaxel@gmail.com>
2024-05-16 19:05:17 -0600

    Cambio a RickandMorty API

--- Proyecto10/js/index.js
+++ Proyecto10/js/index.js
@@ -4,29 +4,28 @@ let listaProductos = document.querySelector('#listaProductos');
 // Obtiene produtos
 let obtieneProductos = () => {
     // Promise o Promesa
-    fetch('https://fakestoreapi.com/products/')
+    fetch('https://rickandmortyapi.com/api/character/?page=1')
         .then(res => res.json())
-        .then(productosObtenidos => {
-            console.log("Productos obtenidos ", productosObtenidos);
+        .then(personajesObtenidos => {
+            console.log("Personajes ", personajesObtenidos.results);
 
-            productosObtenidos.forEach((producto, indice) => {
-                console.log("Producto ", producto);
-
+            personajesObtenidos.results.forEach(personaje => {
                 listaProductos.innerHTML += `
                 <div class="col-12 col-md-3">
                     <div class="card" style="width: 18rem;">
-                        <img src="${producto.image}" class="card-img-top imagenProducto py-2" alt="">
+                        <img src="${personaje.image}" class="card-img-top imagenProducto py-2" alt="">
                         <div class="card-body">
-                            <h5 class="card-title">${producto.title}</h5>
-                            <p class="card-text">${producto.description.slice(0,100)}</p>
-                            <p class="text-danger">$ ${producto.price}</p>
-                            <a href="#" class="btn btn-primary">Comprar</a>
+                            <h5 class="card-title">Nombre: ${personaje.name}</h5>
+                            <p class="card-text">Especie: ${personaje.species}</p>
+                            <p class="text-text">Género: ${personaje.gender}</p>
+                            <p class="text-text">Localización: ${personaje.location.name}</p>
+                            <p class="text-danger">Estado: ${personaje.status}</p>
                         </div>
                     </div>
                 </div>
-                `;
-            });
-        });
-}
+            `;
+            })
+        })
+};
 
 obtieneProductos();
